02e9e2a5d15e9e81bc0a8473d1985eef6fc3fe52
Mohammad Sami Bhat <muhammad.22scse1010005@galgotiasuniversity.edu.in>
2024-06-02 00:18:38 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort/merge-sort.cpp
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int m, int r)
+    {
+         // Your code here
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    vector<int> L(n1), R(n2);
+
+   
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+     for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+     int i = 0, j = 0, k = l;
+    
+      while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+         k++;
+    }
+
+   
+      while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+   
+      while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+        
+    }
+    
+   }
+    
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+         if (l < r) {
+       
+        int m = l + (r - l) / 2;
+
+    
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+        
+    }
+   }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+    
+    scanf("%d",&n);
+    int arr[n+1];
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.mergeSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
