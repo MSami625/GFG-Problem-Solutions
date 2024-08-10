cd0aa0bde51dd9f43edb46afd5eefc6062930aa9
Mohammad Sami Bhat <muhammad.22scse1010005@galgotiasuniversity.edu.in>
2024-08-10 20:44:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of first n terms/sum-of-first-n-terms.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+  long long sum=0;
+    long long sumOfSeries(long long n) {
+        // code here
+        
+       if(n>0){
+       sum+=n*n*n;
+       sumOfSeries(n-1);
+       }else return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.sumOfSeries(N) << "\n";
+    }
+}
+// } Driver Code Ends
